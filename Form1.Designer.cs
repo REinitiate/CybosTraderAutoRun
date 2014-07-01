1c33429a6b0f00308022b8ae573cef8b0f5bf6dd
REinitiate <reinitiate@gmail.com>
2014-07-01 13:53:51 +0900

    이거저거 수정

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,13 +35,15 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonGetColor = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonItemClick
             // 
-            this.buttonItemClick.Location = new System.Drawing.Point(210, 100);
+            this.buttonItemClick.Location = new System.Drawing.Point(240, 125);
+            this.buttonItemClick.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonItemClick.Name = "buttonItemClick";
-            this.buttonItemClick.Size = new System.Drawing.Size(100, 23);
+            this.buttonItemClick.Size = new System.Drawing.Size(114, 29);
             this.buttonItemClick.TabIndex = 0;
             this.buttonItemClick.Text = "버튼클릭";
             this.buttonItemClick.UseVisualStyleBackColor = true;
@@ -49,16 +51,18 @@
             // 
             // textBoxItemClick
             // 
-            this.textBoxItemClick.Location = new System.Drawing.Point(210, 39);
+            this.textBoxItemClick.Location = new System.Drawing.Point(240, 49);
+            this.textBoxItemClick.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBoxItemClick.Name = "textBoxItemClick";
-            this.textBoxItemClick.Size = new System.Drawing.Size(100, 21);
+            this.textBoxItemClick.Size = new System.Drawing.Size(114, 25);
             this.textBoxItemClick.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(210, 129);
+            this.button1.Location = new System.Drawing.Point(240, 161);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.Size = new System.Drawing.Size(114, 29);
             this.button1.TabIndex = 2;
             this.button1.Text = "텍스트입력";
             this.button1.UseVisualStyleBackColor = true;
@@ -66,16 +70,18 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(210, 12);
+            this.textBox2.Location = new System.Drawing.Point(240, 15);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.Size = new System.Drawing.Size(114, 25);
             this.textBox2.TabIndex = 4;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(13, 12);
+            this.button2.Location = new System.Drawing.Point(15, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(191, 21);
+            this.button2.Size = new System.Drawing.Size(218, 26);
             this.button2.TabIndex = 5;
             this.button2.Text = "매크로동작";
             this.button2.UseVisualStyleBackColor = true;
@@ -84,27 +90,38 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(235, 82);
+            this.label1.Location = new System.Drawing.Point(269, 102);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.Size = new System.Drawing.Size(52, 15);
             this.label1.TabIndex = 6;
             this.label1.Text = "테스트";
             // 
             // buttonGetColor
             // 
-            this.buttonGetColor.Location = new System.Drawing.Point(210, 158);
+            this.buttonGetColor.Location = new System.Drawing.Point(240, 198);
+            this.buttonGetColor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonGetColor.Name = "buttonGetColor";
-            this.buttonGetColor.Size = new System.Drawing.Size(100, 23);
+            this.buttonGetColor.Size = new System.Drawing.Size(114, 29);
             this.buttonGetColor.TabIndex = 7;
             this.buttonGetColor.Text = "픽셀가져오기";
             this.buttonGetColor.UseVisualStyleBackColor = true;
             this.buttonGetColor.Click += new System.EventHandler(this.buttonGetColor_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 317);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "By REinitiate";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 273);
+            this.ClientSize = new System.Drawing.Size(368, 341);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonGetColor);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -112,6 +129,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBoxItemClick);
             this.Controls.Add(this.buttonItemClick);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -129,6 +147,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonGetColor;
+        private System.Windows.Forms.Label label2;
     }
 }
 
